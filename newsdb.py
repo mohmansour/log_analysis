73901ccb8f4ed7e53b52e73ab9baba84b00c8383
mohamed mansour <mohamed.mnsoor_1995@yahoo.com>
2018-02-25 18:44:38 +0200

    thir

--- newsdb.py
+++ newsdb.py
@@ -13,32 +13,32 @@ def connection():
 
 
 def popular_articles():
-  c = connection()
-  c.execute("""
+    c = connection()
+    c.execute("""
             SELECT title , COUNT(*) AS num FROM log,
             articles  WHERE slug = SUBSTRING(path FROM 10)
             GROUP BY title ORDER BY num DESC LIMIT 3
             """)
-  return c
+    return c
 
 
 def print_popular_articles():
     print("articles:")
     for items in popular_articles():
-        print(str(items[0]) +" -- " + str(items[1]) + " views ")
+        print(str(items[0]) + " -- " + str(items[1]) + " views ")
 
 
 # define the popular authors
 
 def popular_authors():
-  c = connection()
-  c.execute("""
+    c = connection()
+    c.execute("""
             SELECT name, COUNT(*) AS num FROM log,articles,
             authors WHERE slug = SUBSTRING(path FROM 10)
             AND author=author AND author = authors.id 
             GROUP BY name ORDER BY num DESC;
             """)
-  return c
+    return c
 
 
 def print_popular_authors():
@@ -47,12 +47,12 @@ def print_popular_authors():
         print(str(item[0]) + " -- " + str(item[1]) + " views ")
 
 
-#Return days with error more than 1%
+# Return days with error more than 1%
 
 
 def more_errors():
-  c = connection()
-  c.execute("""
+    c = connection()
+    c.execute("""
             WITH errlog AS(SELECT DATE(time) AS erTime,
             ROUND ((SUM (CASE WHEN SUBSTRING(status,0,4)::
             INTEGER >=400 THEN 1 ELSE 0 END ) * 100)
@@ -63,13 +63,13 @@ def more_errors():
             AS date FROM errlog GROUP BY errlog.total ,
             errlog.erTime HAVING errlog.total > 1;
             """)
-  return c
+    return c
 
 
 def print_more_errors():
     print("Errors:")
     for item in more_errors():
-        print(  str(item[1]) + " -- " + str(item[0]) + " error ")
+        print(str(item[1]) + " -- " + str(item[0]) + " error ")
 
 
 # printing output of each function
